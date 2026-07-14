530e460bca8d5b263782f558831f1caf8a67f94e
sobhan661 <sobhantheimpaler@gmail.com>
2026-07-14 22:09:01 +0330

    Fix importing bug

--- webapp/core/urls.py
+++ webapp/core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.shortcuts import redirect
+from pathlib import Path
 
-from views import Home
+from core.views import Home
 
 urlpatterns = [
-    path('', lambda x: redirect('home')),
+    path('', lambda x: redirect('/home')),
     path('home', Home.as_view())
 ]
+
